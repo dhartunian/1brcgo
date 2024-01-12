78aa079dfb6df823a7eb2e9db827249380bb474e
Marcus Gartner <marcus@cockroachlabs.com>
2024-01-12 12:13:02 -0500

    Add encrypt.go and decrypt_and_compile.go
    To encrypt your submission, run `go run encrypt.go -- name.go`.

---
+++ decrypt_and_compile.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"os/exec"
+)
+
+func main() {
+	flag.Parse()
+	fileName := flag.Arg(0)
+	encFileName := fileName + ".enc"
+	keyFileName := fileName + ".key"
+
+	// Decrypt the symmetric key.
+	execCmd("openssl", "rsautl", "-decrypt", "-inkey", "private_key.pem", "-in", keyFileName, "-out", "key.sym")
+	defer execCmd("rm", "key.sym")
+
+	// Decrypt the file.
+	execCmd("openssl", "aes-256-cbc", "-d", "-a", "-pbkdf2", "-in", encFileName, "-out", fileName, "-kfile", "key.sym")
+	defer execCmd("rm", fileName)
+
+	// Compile the file into a binary.
+	execCmd("go", "build", fileName)
+}
+
+func execCmd(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
