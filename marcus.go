2e2ac7fc14ccd92bd13c73224164f7a96f9e9f93
Marcus Gartner <marcus@cockroachlabs.com>
2024-01-18 20:01:29 -0500

    marcus2

--- marcus.go
+++ marcus.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"math"
@@ -12,15 +11,15 @@ import (
 	"runtime/pprof"
 	"slices"
 	"sync"
-	"unsafe"
 )
 
 const (
 	inputFile = "measurements.txt"
 	// Set profile to true to generate CPU and heap profiles.
 	profile = false
-	// Set runTests to run tests at the start of the program.
-	runTests = false
+	// Set runTestsAndAssertions to run tests at the start of the program and
+	// perform runtime assertions.
+	runTestsAndAssertions = false
 	// numGoroutines controls the number of goroutines to spawn to scan the
 	// input file.
 	// The processor has 8 cores + 2 efficiency cores.
@@ -31,9 +30,9 @@ func main() {
 	// Disable GC. We don't need that!
 	debug.SetGCPercent(-1)
 
-	if runTests {
+	if runTestsAndAssertions {
 		// Run tests.
-		TestSplitCityAndTemp()
+		TestParseTemp()
 	}
 
 	if profile {
@@ -125,13 +124,13 @@ func Run() {
 				// Skip the first line unless this is the goroutine reading from
 				// the beginning. The previous goroutine will scan the skipped
 				// line.
-				_, _ = s.Scan()
+				s.SkipFirstLine()
 			}
 			bytesToRead := int(end - start)
-			for line, ok := s.Scan(); ok; line, ok = s.Scan() {
-				city, temp := SplitCityAndTemp(line)
-				rec := m.Lookup(city)
-				rec.Add(temp)
+			for city, temp, ok := s.Scan(); ok; city, temp, ok = s.Scan() {
+				t := ParseTemp(temp)
+				sum := m.Lookup(city)
+				sum.Add(t)
 				// Scan just past the number of bytes to read because the
 				// goroutine reading from the next section will skip the first
 				// line it reads.
@@ -173,7 +172,11 @@ func Run() {
 	// Print the output.
 	w := bufio.NewWriter(os.Stdout)
 	for _, city := range cities {
-		var s Summary
+		s := Summary{
+			key: city,
+			min: math.MaxInt16,
+			max: math.MinInt16,
+		}
 		// Merge all summaries for this city together.
 		for i := range maps {
 			if sub := maps[i].LookupExisting(city); sub != nil {
@@ -190,69 +193,48 @@ func Run() {
 	}
 }
 
-// SplitCityAndTemp splits the given line into a city and temperature and
-// returns both.
-func SplitCityAndTemp(line []byte) (city []byte, _ Temp) {
+// ParseTemp parses a byte slice representing a temperature.
+func ParseTemp(t []byte) Temp {
 	// Gather the key and fetch the corresponding record. We can do this without
 	// scanning the line because there are only four possible positions for
 	// the semicolon. The valid formats for the line are:
 	//
-	//     x;0.0
-	//    x;00.0
-	//    x;-0.0
-	//   x;-00.0
+	//     0.0
+	//    00.0
+	//    -0.0
+	//   -00.0
 	//
 	// We use the limited locations of semicolons and minus characters to avoid
 	// conditional expressions and loops.
-	l := len(line)
-	// In the common case, the line will be more than 5 characters.
-	if l > 5 {
-		// Avoid future bounds checks.
-		_ = line[:l-6]
+	l := len(t)
+	switch l {
+	case 3:
+		tenths := int(t[2] - '0')
+		ones := int(t[0]-'0') * 10
+		return Temp(ones + tenths)
+	case 4:
+		tenths := int(t[3] - '0')
+		ones := int(t[1]-'0') * 10
+		tens := int(t[0]-'0') * 100
 
-		// Gather all the digits. The tenths digit is the last one, the decimal
-		// point is skipped, the ones digit is the third to last character, and
-		// the tens digit is the fourth to last character, if it exists. Further
-		// down we'll zero tens if it was actually a minus or semicolon.
-		tenths := int(line[l-1] - '0')
-		ones := int(line[l-3]-'0') * 10
-		tens := int(line[l-4]-'0') * 100
-
-		// neg4 is 1 if a minus character is present at len-4, and 0 otherwise.
-		// neg5 is 1 if a minus character is present at len-5, and 0 otherwise.
+		// neg is 1 if the first character is the minus charater, and 0
+		// otherwise.
 		//
 		// NOTE: The Go compiler eliminates jumps when using this form of
 		// conditional.
-		var neg4 int
-		var neg5 int
-		if line[l-4] == '-' {
-			neg4 = 1
-		}
-		if line[l-5] == '-' {
-			neg5 = 1
-		}
-		// neg is 1 if a minus character is present at len-4 or len-5.
-		neg := neg4 | neg5
-
-		// semi4 is 1 if a semi-column is present at len-4, and 0 otherwise.
-		//
-		// NOTE: The Go compiler eliminates jumps when using this form.
-		var semi4 int
-		if line[l-4] == ';' {
-			semi4 = 1
+		var neg int
+		if t[0] == '-' {
+			neg = 1
 		}
 
 		// Clear tens if there was a minus character in that position.
-		tens = tens &^ -neg4
-
-		// Clear tens if there was a semicolon in that position.
-		tens = tens &^ -semi4
+		tens = tens &^ -neg
 
 		// Add the ones and tenths digits.
-		t := ones + tenths
+		temp := ones + tenths
 
 		// Add the tens digit.
-		t += tens
+		temp += tens
 
 		// Negate the value if we found a minus character in len-4 or len-5
 		// position.
@@ -260,104 +242,64 @@ func SplitCityAndTemp(line []byte) (city []byte, _ Temp) {
 		// See "conditionally negate a value without branching", from "Bit
 		// Twiddling Hacks",
 		// https://graphics.stanford.edu/~seander/bithacks.html.
-		t = (t ^ -neg) + neg
+		temp = (temp ^ -neg) + neg
 
-		// TODO: There's another bounds check here even in cases where it's
-		// fairly obvious to me that semiPos is 4, 5, or 6 (the current
-		// commented-out semiPos doesn't make that so obvious, but other ways of
-		// calculating it do). But I can't convince the Go compiler of this, and
-		// the switch with some jumps is faster.
-		//
-		// semi5 is 1 if a semi-column is present at len-4, and 0 otherwise.
-		// var semi5 int
-		// if line[l-5] == ';' {
-		// 	semi5 = 1
-		// }
-		// semiPos1 := 4
-		// semiPos2 := semi5 &^ semi4
-		// semiPos3 := 2 & ^((semi5 | semi4) << 1)
-		// semiPos := l - (semiPos1 + semiPos2 + semiPos3)
-		// return line[:semiPos], Temp(t)
-
-		switch {
-		case line[l-4] == ';':
-			return line[:l-4], Temp(t)
-		case line[l-5] == ';':
-			return line[:l-5], Temp(t)
-		default:
-			return line[:l-6], Temp(t)
+		return Temp(temp)
+	case 5:
+		tenths := int(t[4] - '0')
+		ones := int(t[2]-'0') * 10
+		tens := int(t[1]-'0') * 100
+		temp := tens + ones + tenths
+		temp *= -1
+		return Temp(temp)
+	default:
+		// Impossible.
+		if runTestsAndAssertions {
+			panic(fmt.Sprintf("unexpected temperature slice length %d", l))
 		}
-	} else {
-		// Special case. We only have 5 bytes, which must be in the form
-		// "x;0.0". So the key is the first byte and the semicolon is the
-		// second.
-
-		// Avoid future bounds checks.
-		_ = line[:l-5]
-
-		// Gather the two digits.
-		tenths := int(line[l-1] - '0')
-		ones := int(line[l-3]-'0') * 10
-		t := ones + tenths
-
-		return line[:l-4], Temp(t)
+		return Temp(0)
 	}
 }
 
 const (
-	// Multi-byte characters are rare, so we'll group them in with other
-	// characters by ignoring the most significant bit. Our table can then be
-	// 128x128x128.
+	// ASCII characters are all less than 128. Multi-byte characters, which
+	// start with bytes greater than 128 are rare, so we'll group them in with
+	// other characters by ignoring the most significant bit. Our table can then
+	// be 128x128x128.
 	tableDimension = 128
 )
 
-// SummaryMap is a map from a byte slice key to a Summary.
+// SummaryMap is a map from a 3-byte key to a Summary linked list.
 type SummaryMap struct {
-	// This is a ~120MB table.
-	table [tableDimension][tableDimension][tableDimension]SummaryList
+	table [tableDimension][tableDimension][tableDimension]*Summary
 	keys  []string
 }
 
 // Keys returns all the keys in the map, as strings.
-func (rt *SummaryMap) Keys() []string {
-	return rt.keys
+func (m *SummaryMap) Keys() []string {
+	return m.keys
 }
 
 // LookupExisting returns the Summary in the map associated with the key. If
 // there is no summary matching the key, it returns nil.
-func (rt *SummaryMap) LookupExisting(s string) *Summary {
-	p := unsafe.StringData(s)
-	key := unsafe.Slice(p, len(s))
-
-	b0 := (key[0]) & 127
+func (m *SummaryMap) LookupExisting(key string) *Summary {
+	var b0, b1, b2 byte
+	b0 = key[0] & 127
 	// Use the NULL byte for the second and third characters in the lookup table
 	// if the key is less than 3 bytes long.
-	b1 := byte(0)
-	b2 := byte(0)
-	if len(key) > 2 {
-		b2 = (key[2]) & 127
-	}
 	if len(key) > 1 {
-		b1 = (key[1]) & 127
+		b1 = key[1] & 127
+	}
+	if len(key) > 2 {
+		b2 = key[2] & 127
 	}
 
-	// Use the list with the matching first three bytes.
-	list := &rt.table[b0][b1][b2]
-
-	// Check the first summary in the list.
-	rec := &list.first
-	// The Go compiler does not allocate in this special case. See
-	// https://github.com/golang/go/blob/e9b3ff15/src/bytes/bytes.go#L19
-	if string(key) == rec.key {
-		return rec
-	}
-
-	// Check the other record in the list.
-	for i := range list.others {
-		rec := &list.others[i]
-		if string(key) == rec.key {
-			return rec
+	s := m.table[b0][b1][b2]
+	for s != nil {
+		if key == s.key {
+			return s
 		}
+		s = s.next
 	}
 
 	// The summary was not found.
@@ -368,66 +310,44 @@ func (rt *SummaryMap) LookupExisting(s string) *Summary {
 // exist, a new one is added to the map and returned.
 //
 // The given key must be non-empty.
-func (rt *SummaryMap) Lookup(key []byte) *Summary {
-	b0 := (key[0]) & 127
+func (m *SummaryMap) Lookup(key []byte) *Summary {
+	var b0, b1, b2 byte
+	b0 = key[0] & 127
 	// Use the NULL byte for the second and third characters in the lookup table
 	// if the key is less than 3 bytes long.
-	b1 := byte(0)
-	b2 := byte(0)
-	if len(key) > 2 {
-		b2 = (key[2]) & 127
-	}
 	if len(key) > 1 {
-		b1 = (key[1]) & 127
+		b1 = key[1] & 127
+	}
+	if len(key) > 2 {
+		b2 = key[2] & 127
 	}
 
-	// Use the list with the matching first three bytes.
-	list := &rt.table[b0][b1][b2]
-
-	// Check the first summary in the list.
-	rec := &list.first
-	// The Go compiler does not allocate in this special case. See
-	// https://github.com/golang/go/blob/e9b3ff15/src/bytes/bytes.go#L19
-	if string(key) == rec.key {
-		return rec
-	}
-
-	// Check the other record in the list.
-	for i := range list.others {
-		rec := &list.others[i]
-		if string(key) == rec.key {
-			return rec
+	s := m.table[b0][b1][b2]
+	last := s
+	for s != nil {
+		// The Go compiler does not allocate in this special case. See
+		// https://github.com/golang/go/blob/e9b3ff15/src/bytes/bytes.go#L19
+		if string(key) == s.key {
+			return s
 		}
+		last = s
+		s = s.next
 	}
 
 	// The summary was not found. Allocate a string for the key, and add a new
 	// summary to the map.
-	city := string(key)
-	rt.keys = append(rt.keys, city)
-	return list.Append(city)
-}
-
-// SummaryList is a list of Summary structs.
-type SummaryList struct {
-	// TODO(mgartner): Not sure this fast-path really does anything. Test it.
-	first  Summary
-	others []Summary
-}
-
-// Append adds a new Summary to the list with the given key and returns the
-// newly created Summary.
-func (l *SummaryList) Append(key string) *Summary {
-	r := Summary{
-		key: key,
+	s = &Summary{
+		key: string(key),
 		min: math.MaxInt16,
 		max: math.MinInt16,
 	}
-	if len(l.first.key) == 0 {
-		l.first = r
-		return &l.first
+	if last != nil {
+		last.next = s
+	} else {
+		m.table[b0][b1][b2] = s
 	}
-	l.others = append(l.others, r)
-	return &l.others[len(l.others)-1]
+	m.keys = append(m.keys, s.key)
+	return s
 }
 
 // Temp represents a temperature, stored as an integer 10x the temperature.
@@ -448,10 +368,9 @@ func (t TempSum) AsFloat() float64 {
 }
 
 // Summary summarizes all temperature recordings for a given key.
-//
-// NOTE: The order of fields is important for maintaining a struct of 32 bytes.
 type Summary struct {
 	key   string
+	next  *Summary
 	sum   TempSum
 	count uint32
 	min   Temp
@@ -466,7 +385,7 @@ func (r *Summary) Add(t Temp) {
 	r.count++
 }
 
-// Merge merges other with the record.
+// Merge merges other with the Summary.
 func (r *Summary) Merge(other *Summary) {
 	r.min = min(r.min, other.min)
 	r.max = max(r.max, other.max)
@@ -490,13 +409,14 @@ func (r Summary) Avg() float64 {
 }
 
 // Scanner is copied from bufio.Scanner, with bunch of not-so-necessary
-// funcationality ripped out.
+// functionality ripped out.
 type Scanner struct {
 	r                 io.Reader // The reader provided by the client.
 	buf               []byte    // Buffer used as argument to split.
 	start             int       // First non-processed byte in buf.
 	end               int       // End of data in buf.
 	totalBytesScanned int
+	firstLineRead     bool
 }
 
 // Init initializes the scanner with the given buffer.
@@ -505,37 +425,53 @@ func (s *Scanner) Init(r io.Reader, buf []byte) {
 	s.buf = buf
 }
 
+// SkipFirstLine scans until the first newline in the io.Reader.
+func (s *Scanner) SkipFirstLine() {
+	if s.firstLineRead {
+		panic("cannot call SkipFirstLine multiple times not after Scan")
+	}
+	s.firstLineRead = true
+	// Perform the first read.
+	n, err := s.r.Read(s.buf)
+	s.end += n
+	if err != nil {
+		panic(err)
+	}
+	advance := scanLine(s.buf)
+	if !s.advance(advance) {
+		panic("could not advance")
+	}
+}
+
 // Scan scans the io.Reader for the next line and returns it.
-func (s *Scanner) Scan() (line []byte, ok bool) {
-	// Loop until we have a line.
+func (s *Scanner) Scan() (city []byte, temp []byte, ok bool) {
+	s.firstLineRead = true
+	// Loop until we have a full line.
 	for {
 		// See if we can get a line with what we already have.
-		// If we've run out of data but have an error, give the split function
-		// a chance to recover any remaining, possibly empty line.
 		if s.end > s.start {
 			var advance int
-			advance, line = scanLines(s.buf[s.start:s.end])
+			advance, city, temp = scanCityAndTemp(s.buf[s.start:s.end])
 			if !s.advance(advance) {
-				return nil, false
+				return nil, nil, false
 			}
-			if line != nil {
+			if city != nil {
 				s.totalBytesScanned += advance
-				return line, true
+				return city, temp, true
 			}
 		}
-		// Must read more data.
-		// First, shift data to beginning of buffer if there's lots of empty space
-		// or space is needed.
-		if s.start > 0 && (s.end == len(s.buf) || s.start > len(s.buf)/2) {
+		// Must read more data. First, shift data to the beginning of the
+		// buffer.
+		if s.start > 0 {
 			copy(s.buf, s.buf[s.start:s.end])
 			s.end -= s.start
 			s.start = 0
 		}
-		// Finally we can read some input.
+		// Read some input.
 		n, err := s.r.Read(s.buf[s.end:len(s.buf)])
 		s.end += n
 		if err != nil {
-			return nil, false
+			return nil, nil, false
 		}
 	}
 }
@@ -557,52 +493,67 @@ func (s *Scanner) advance(n int) bool {
 	return true
 }
 
-func scanLines(data []byte) (advance int, token []byte) {
-	if i := bytes.IndexByte(data, '\n'); i >= 0 {
-		// We have a full newline-terminated line.
-		return i + 1, data[0:i]
+func scanLine(data []byte) (advance int) {
+	for i := range data {
+		if data[i] == '\n' {
+			return i + 1
+		}
 	}
-	// Request more data.
-	return 0, nil
+	return 0
+}
+
+func scanCityAndTemp(data []byte) (advance int, city []byte, temp []byte) {
+	i := 0
+	for ; i < len(data); i++ {
+		if data[i] == ';' {
+			city = data[:i]
+			break
+		}
+	}
+	cityEnd := i
+	// The temperature is at least 3 bytes, e.g., "0.0", so skip ahead 4 bytes
+	// to start looking for the newline.
+	i += 4
+	for ; i < len(data); i++ {
+		if data[i] == '\n' {
+			temp = data[cityEnd+1 : i]
+			return i + 1, city, temp
+		}
+	}
+	return 0, nil, nil
 }
 
 // /////////////////////////////////////////////////////////
 // Tests
 // /////////////////////////////////////////////////////////
 
-func TestSplitCityAndTemp() {
+func TestParseTemp() {
 	type testCase struct {
 		input        []byte
-		expectedCity []byte
-		expectedTemp Temp
+		expectedTemp float64
 	}
 	testCases := []testCase{
-		{[]byte("x;1.2"), []byte("x"), 12},
-		{[]byte("x;-1.2"), []byte("x"), -12},
-		{[]byte("x;12.3"), []byte("x"), 123},
-		{[]byte("x;-12.3"), []byte("x"), -123},
-		{[]byte("foo;99.9"), []byte("foo"), 999},
-		{[]byte("foo;-99.9"), []byte("foo"), -999},
-		{[]byte("abcdefghijklmnop;0.6"), []byte("abcdefghijklmnop"), 6},
-		{[]byte("abcdefghijklmnop;-0.6"), []byte("abcdefghijklmnop"), -6},
-		{[]byte("abcdefghijklmnop;12.6"), []byte("abcdefghijklmnop"), 126},
-		{[]byte("abcdefghijklmnop;-12.6"), []byte("abcdefghijklmnop"), -126},
-		{[]byte("é;8.9"), []byte("é"), 89},
-		{[]byte("é;78.9"), []byte("é"), 789},
-		{[]byte("é;-8.9"), []byte("é"), -89},
-		{[]byte("é;-78.9"), []byte("é"), -789},
-		{[]byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº;5.6"), []byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº"), 56},
-		{[]byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº;45.6"), []byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº"), 456},
-		{[]byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº;-5.6"), []byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº"), -56},
-		{[]byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº;-45.6"), []byte("üøª∂˙ßªß∂˙ªº∂ß∂˙ªº"), -456},
+		{[]byte("0.0"), 0},
+		{[]byte("0.1"), 0.1},
+		{[]byte("0.5"), 0.5},
+		{[]byte("0.9"), 0.9},
+		{[]byte("1.2"), 1.2},
+		{[]byte("5.5"), 5.5},
+		{[]byte("9.9"), 9.9},
+		{[]byte("-1.2"), -1.2},
+		{[]byte("-5.5"), -5.5},
+		{[]byte("-9.9"), -9.9},
+		{[]byte("13.2"), 13.2},
+		{[]byte("56.5"), 56.5},
+		{[]byte("98.9"), 98.9},
+		{[]byte("-13.2"), -13.2},
+		{[]byte("-56.5"), -56.5},
+		{[]byte("-98.9"), -98.9},
 	}
 	for _, tc := range testCases {
-		city, temp := SplitCityAndTemp(tc.input)
-		if string(city) != string(tc.expectedCity) {
-			panic(fmt.Sprintf("expected key %q, got %q", tc.expectedCity, city))
-		}
-		if temp != tc.expectedTemp {
-			panic(fmt.Sprintf("expected temp %d, got %d", tc.expectedTemp, temp))
+		temp := ParseTemp(tc.input)
+		if temp.AsFloat() != tc.expectedTemp {
+			panic(fmt.Sprintf("expected temp %d, got %d", tc.expectedTemp, temp.AsFloat()))
 		}
 	}
 }
